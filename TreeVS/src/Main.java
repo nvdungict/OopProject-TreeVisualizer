b614dca98958117578ed690a83b6fd8f60284f21
Shirou191 <emiyashirou191@gmail.com>
2024-06-09 21:32:48 +0700

    upload 09/06/2024

---
+++ TreeVS/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+import views.MainMenu;
+import views.TreeVisualizer;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Invoke the MainMenu class to display the menu
+        new MainMenu();
+    }
+}
